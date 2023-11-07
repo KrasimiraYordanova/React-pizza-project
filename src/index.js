f307102096ec30350c336f65a4d50e6800691771
krasimiranova <krasy_nova@yahoo.com>
2023-11-07 20:40:00 +0100

    props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
 
 const pizzaData = [
     {
@@ -49,26 +50,72 @@ const pizzaData = [
 
 function App() {
     return (
-      <div>
-        <h1>Hello React!</h1>
-        <Pizza />
-        <Pizza />
-        <Pizza />
+      <div className="container">
+        <Header />
+        <Menu />
+        <Footer />
       </div>
+    );
+}
+
+function Header() {
+    return <header className="header"><h1 style={{color:'inherit', fontSize: "3rem"}}>Reactive pizza Co.</h1></header>
+}
+
+function Menu() {
+  const pizzas = pizzaData;
+  const pizzasQty = pizzas.length;
+    return (
+      <main>
+        <section className="menu">
+            <h2>Our menu</h2>
+
+          {/* when using short cercuting our condition should always evlutate to true or false, cannot have an actual value */}
+            {pizzasQty > 0 && (<ul className="pizzas">
+              {pizzas.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name} />)}
+            </ul>)}
+            
+
+            {/* {<div>
+                <Pizza name="Pizza Spinaci" ingredients="Tomato, mozarella, spinach, and ricotta cheese" photoName="pizzas/spinaci.jpg" price={11} />
+                <Pizza name="Pizza Funghi" ingredients="Tomato, mushrooms" photoName="pizzas/funghi.jpg" price={12} />
+            </div>} */}
+        </section>
+      </main>
     );
 }
 
 // in React we create new componants writing functions - each componant can return exactely one element
 // 1. function need to start with uppercase
 // 2. function needs to return some mark-up
-function Pizza() {
-    return (
+function Pizza(props) {
+  console.log(props);
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h2>Pizza Spinaci</h2>
-        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    );
+    </li>
+  );
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const open = 12;
+    const close = 21;
+    const isOpen = hour >= open && hour <= close
+   
+    return (<footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We are open untill {close}:00.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>)
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
